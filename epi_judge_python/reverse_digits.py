8b288baf9c54bed412991cb9e4292b414f21540a
Dileep Pandey <dileep.pandey@asu.edu>
2019-03-15 00:36:26 -0700

    epi-1

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
+from math import log10
+from math import floor
 
 
 def reverse(x):
     # TODO - you fill in here.
-    return 0
+    result = 0
+    is_negative = False
+    if x < 0:
+        is_negative = True
+    x = abs(x)
+    while x > 0:
+        result += (10 ** floor(log10(x))) * (x % 10)
+        x //= 10
+    return result if not is_negative else -1 * result
 
 
 if __name__ == '__main__':
