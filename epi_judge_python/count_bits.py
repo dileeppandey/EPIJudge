62583d39d83868ae52b16625c17d1bcbc20b4c84
Dileep Pandey <Dileep.Pandey@asu.edu>
2020-05-10 10:20:48 -0700

    count bits --bitwise

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -5,7 +5,16 @@ def count_bits(x: int) -> int:
     return sum([int(ch) for ch in bin(x)[2:]])
 
 
+def count_bits_bitwise(x: int) -> int:
+    count = 0
+    while x != 0:
+        count = count + 1 if x & 1 == 1 else count
+        x = x >> 1
+    return count
+
+
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+    generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
+                                   count_bits)
+    generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
+                                   count_bits_bitwise)
